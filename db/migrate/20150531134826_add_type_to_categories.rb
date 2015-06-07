81d590efdf09423858fde68cb78e16bcb496ba64
12look <waplookoff@gmail.com>
2015-06-07 19:43:26 +0300

    Update isvideo feild migration

--- db/migrate/20150531134826_add_type_to_categories.rb
+++ db/migrate/20150531134826_add_type_to_categories.rb
@@ -1,5 +1,5 @@
 class AddTypeToCategories < ActiveRecord::Migration
   def change
-    add_column :categories, :isvideo, :boolean, :default => 0
+    add_column :categories, :isvideo, :boolean, :default => false
   end
 end
