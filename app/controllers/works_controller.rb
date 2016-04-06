56e810811a1d156692438d152113755e3621a4bc
12look <waplookoff@gmail.com>
2016-04-07 00:24:07 +0300

    Edit works permitted params

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -140,7 +140,7 @@ class WorksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def work_params
       params.require(:work).permit(
-          :name, :description, :user_id, :category_id, :file,
+          :name, :description, :user_id, :category_id, :file, :archive_url,
           images_attributes: [ :image, :_destroy, :id ])
     end
 end
