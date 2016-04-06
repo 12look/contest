1b87094bfafdfbb7d8abd8ad44a41e290b8518bd
12look <waplookoff@gmail.com>
2016-04-07 02:17:52 +0300

    Change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# [:admin, :jury, :participant].each do |role|
-#   Role.find_or_create_by( name: role )
-# end
+[:admin, :jury, :participant].each do |role|
+  Role.find_or_create_by( name: role )
+end
 # @user = User.find_by_email("test@example.com")
 # @user.add_role :juryAdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 #AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+AdminUser.create!(email: 'waplookoff@gmail.com', password: 'n18nM3{X&{Ux}]u', password_confirmation: 'n18nM3{X&{Ux}]u')
